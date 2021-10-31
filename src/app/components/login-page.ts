a941a13476cc04814d7c719a351a855c9aa6147a
Andre Ferraz <andreferraz25384@gmail.com>
2021-10-31 14:42:49 -0300

    Darken background image in the login page.

--- src/app/components/login-page.ts
+++ src/app/components/login-page.ts
@@ -1,30 +1,32 @@
 import { Component } from './component';
 
 const baseTemplate = `
-  <div class="content">
-    <h1 class="title">Chat App</h1>
-    
-    <div class="field-wrapper">
-      <span class="label">Nome</span>
-      <input id="username" class="input-username" type="text" />
+  <div class="dark">
+    <div class="content">
+      <h1 class="title">Chat App</h1>
+      
+      <div class="field-wrapper">
+        <span class="label">Nome</span>
+        <input id="username" class="input-username" type="text" />
+      </div>
+      
+      <div class="field-wrapper">
+        <span class="label">Room</span>
+        <select id="rooms" class="select-rooms">
+          <option value="" disabled selected>Select the room</option>
+          <option value="lobby">Lobby</option>
+          <option value="2">Room 2</option>
+          <option value="3">Room 3</option>
+        </select>
+      </div>
+      
+      <button id="btnContinue" class="btn-continue" type="button">Continue</button>
+      
+      <a class="link-github" href="https://github.com/andreferrazz" target="_blank">
+        <img src="/src/assets/images/GitHub-Mark-32px.png" />
+        <span>Check the source code on Github</span>
+      <a/>
     </div>
-    
-    <div class="field-wrapper">
-      <span class="label">Room</span>
-      <select id="rooms" class="select-rooms">
-        <option value="" disabled selected>Select the room</option>
-        <option value="lobby">Lobby</option>
-        <option value="2">Room 2</option>
-        <option value="3">Room 3</option>
-      </select>
-    </div>
-    
-    <button id="btnContinue" class="btn-continue" type="button">Continue</button>
-    
-    <a class="link-github" href="https://github.com/andreferrazz" target="_blank">
-      <img src="/src/assets/images/GitHub-Mark-32px.png" />
-      <span>Check the source code on Github</span>
-    <a/>
   </div>
 `
 
